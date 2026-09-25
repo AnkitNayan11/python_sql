e0809d09f84e2a5aad7478ab1131dd752eb35ede
ankit_nayan11 <88632071+AnkitNayan11@users.noreply.github.com>
2026-09-25 14:15:43 +0530

    Update database connection to remove password
    Removed password from database connection string for security.

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import psycopg2
 import pandas as pd
 from openpyxl.workbook import Workbook
 # 
-db = psycopg2.connect(database="AssignmentPythonSQL", user="postgres", password="Rajat@98")
+db = psycopg2.connect(database="AssignmentPythonSQL", user="postgres", password="")
 
 try:
     data = pd.read_sql("SELECT e.empno ,e.ename, m .ename manager FROM emp e LEFT  JOIN emp m   ON m.empno = e.mgr", con=db)  # Storing Query result in DataFrames
